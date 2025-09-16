af374bad65b61b4c8b0107e44fee2731292330e9
Ismael Barea Insua <80041279+quantosh@users.noreply.github.com>
2025-09-16 18:37:02 +0200

    Update ps1

--- scripts/build-release.ps1
+++ scripts/build-release.ps1
@@ -1,5 +1,5 @@
 # build-release.ps1
-# Uso: .\build-release.ps1 chrome|edge|firefox
+# Usage: .\build-release.ps1 chrome|edge|firefox
 
 param(
     [Parameter(Mandatory=$true)]
@@ -7,8 +7,12 @@ param(
     [string]$target
 )
 
+$repoRoot = Get-Location
+
 $manifest = "manifest.$target.json"
 $dest = "manifest.json"
+$zipName = "Easy.Compressor-$target.zip"
+$destinationPath = Join-Path $repoRoot $zipName
 
 if (!(Test-Path $manifest)) {
     Write-Error "No manifest found for $target ($manifest)"
@@ -17,16 +21,15 @@ if (!(Test-Path $manifest)) {
 
 Copy-Item $manifest $dest -Force
 
-$zipName = "Easy.Compressor-$target.zip"
-
-$destinationPath = Join-Path (Get-Location) $zipName
-
 $itemsToExclude = @("*.zip", "build-release.ps1", "manifest.chrome.json", "manifest.edge.json", "manifest.firefox.json")
-$itemsToCompress = Get-ChildItem -Path . -Recurse | Where-Object { 
-    $_.Name -notin $itemsToExclude -and $_.FullName
+$itemsToCompress = Get-ChildItem -Path $repoRoot -Recurse | Where-Object { 
+    $_.Name -notin $itemsToExclude -and $_.FullName -notmatch "manifest.json"
 }
 
-if (Test-Path $destinationPath) { Remove-Item $destinationPath }
+if (Test-Path $destinationPath) { 
+    Remove-Item $destinationPath 
+}
+
 Compress-Archive -Path $itemsToCompress.FullName -DestinationPath $destinationPath -CompressionLevel Optimal
 
 Write-Host "Release package created: $destinationPath"
